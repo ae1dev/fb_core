a5e865f908557b280cc78e924f763c29375306fb
Elias <elias@deuss.dev>
2025-05-28 23:02:06 +0200

    feat(auth): grab token from cookie

--- lib/endpoints/auth.dart
+++ lib/endpoints/auth.dart
@@ -13,7 +13,7 @@ class AuthEnd extends EndpointBase {
     required String recaptchaToken,
     required String type,
   }) async {
-    final Map map = (await dio.post(
+    Response response = await dio.post(
       'https://auth.featurebase.app/api/v1/auth/login',
       data: {
         "email": email,
@@ -21,8 +21,23 @@ class AuthEnd extends EndpointBase {
         "recaptchaToken": recaptchaToken,
         "type": type,
       },
-    ))
-        .data;
-    return fb.UserWithAuth.fromJson(map['user']);
+    );
+
+    //Set base user
+    Map tempMap = response.data['user'];
+
+    //Update access token from cookie
+    if (response.headers.value('set-cookie') != null) {
+      String setCookie = response.headers.value('set-cookie')!;
+      // Extract the token value using a regular expression
+      final RegExp regExp = RegExp(r'featurebase-access=([^;]+)');
+      final Match? match = regExp.firstMatch(setCookie);
+      if (match != null) {
+        String cookieToken = match.group(1)!;
+        tempMap['accessToken'] = cookieToken;
+      }
+    }
+
+    return fb.UserWithAuth.fromJson(response.data['user']);
   }
 }
